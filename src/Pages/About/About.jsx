c79a7d6f7d8fe14b0d17ab20b46f3274bf421d7f
pharmakarios1 <akhigbeblessed@gmail.com>
2024-11-26 11:06:12 +0100

    refactored feature component

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Format from "../../components/Format/Format";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <Format>About</Format>
+    </div>
+  );
 };
 
 export default About;
